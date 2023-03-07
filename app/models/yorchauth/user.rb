54ffe422e4074e4c800e91f83d711c48c61c1476
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2023-03-06 23:37:57 -0600

    Removing User model

--- app/models/yorchauth/user.rb
+++
@@ -1,8 +0,0 @@
-module Yorchauth
-  class User < ApplicationRecord
-    has_secure_password
-    validates :email, presence: true, uniqueness: true
-    validates :password, presence: true, length: { minimum: 8 }
-    validates :password_confirmation, presence: true, length: { minimum: 8 }
-  end
-end
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,12 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_04_020707) do
-  create_table "yorchauth_users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.0].define(version: 0) do
 end
